3f8d3ce37e87b525b2f35e9e22f66646970d1cb1
Marko Ljubic <m.ljubic9802@gmail.com>
2024-10-26 18:53:39 +0200

    Project set up, server implementation

---
+++ src/main/java/framework/response/Response.java
@@ -0,0 +1,15 @@
+package framework.response;
+
+import framework.request.Header;
+
+public abstract class Response {
+
+    protected Header header;
+
+    public Response() {
+        this.header = new Header();
+    }
+
+    public abstract String render();
+
+}
